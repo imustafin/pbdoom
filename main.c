4ac59d439e280b9f1d9aeeca96274a6293cf8d81
Ilgiz Mustafin <ilgimustafin@gmail.com>
2022-03-19 23:25:48 +0300

    Set DOOMWADDIR for correct version identification

--- main.c
+++ main.c
@@ -77,12 +77,8 @@ static int main_handler(int event_type, int a, int b)
 
 int main (int argc, char* argv[])
 {
-  new_argv = malloc((argc + 3) * sizeof(*new_argv));
-  memmove(new_argv, argv, sizeof(*new_argv) * argc);
-  new_argv[argc] = "-files";
-  new_argv[argc + 1] = "/mnt/ext1/DOOM2.WAD";
-  new_argv[argc + 2] = 0;
-  new_argc = argc + 2;
+  new_argc = argc;
+  new_argv = argv;
 
   InkViewMain(main_handler);
 
