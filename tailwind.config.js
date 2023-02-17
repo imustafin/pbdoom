41335e4ff27aa8eeae860a8591a403f7a664fe9c
imustafin <imustafin@users.noreply.github.com>
2023-02-17 17:32:25 +0000

    deploy: 881e3a38a1c5c6273d2e7a3d122305e4dc7e7968

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+    './node_modules/flowbite/**/*.js'
+  ],
+  theme: {
+    theme: {
+      extend: {},
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('flowbite/plugin')
+  ]
+}
